74c653c28c67067fe11adbae09b077152177cb01
cassiatechnology <technologycassia@gmail.com>
2025-08-06 22:51:38 -0300

    GET Products

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, status
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import async_session
 from app.schemas.product import ProductCreate, ProductRead
-from app.services.product_service import create_product
+from app.services.product_service import create_product, get_product_by_id, list_products
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
@@ -13,3 +14,14 @@ async def get_db():
 @router.post("/", response_model=ProductRead, status_code=status.HTTP_201_CREATED)
 async def create(product: ProductCreate, db: AsyncSession = Depends(get_db)):
     return await create_product(db, product)
+
+@router.get("/", response_model=List[ProductRead])
+async def get_all_products(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_db)):
+    return await list_products(db, skip=skip, limit=limit)
+
+@router.get("/{product_id}", response_model=ProductRead)
+async def get_product(product_id: int, db: AsyncSession = Depends(get_db)):
+    product = await get_product_by_id(db, product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -9,3 +9,15 @@ async def create_product(db: AsyncSession, product_in: ProductCreate) -> Product
     await db.commit()
     await db.refresh(product)
     return product
+
+async def list_products(db: AsyncSession, skip: int = 0, limit: int = 10) -> list[Product]:
+    result = await db.execute(
+        select(Product).offset(skip).limit(limit)
+    )
+    return result.scalars().all()
+
+async def get_product_by_id(db: AsyncSession, product_id: int) -> Product | None:
+    result = await db.execute(
+        select(Product).where(Product.id == product_id)
+    )
+    return result.scalar_one_or_none()
